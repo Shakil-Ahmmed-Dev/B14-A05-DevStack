db142aafa64f6849c735f4dad9be91c165faa546
Shakil Ahmmed <shakilahmmedwakil.dev@gmail.com>
2026-09-14 17:04:52 +0600

    feat: implement landing page header component with navigation links and font styling

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const NAV_LINKS = {
+  header: ["Home", "Technologies", "Projects", "About", "Contact"],
+  footer: {
+    social: ["Github", "Twitter", "Linkedin"],
+    product: ["Home", "Technologies", "Projects"],
+    company: ["About", "Contact", "Careers"],
+  },
+};
--- src/features/landing-page/components/header/index.tsx
+++ src/features/landing-page/components/header/index.tsx
@@ -1,6 +1,8 @@
+import { NAV_LINKS } from "@/constants";
+
 export default function Header() {
   return (
-    <header>
+    <header className="pt-6 max-w-7xl mx-auto flex justify-between items-center">
       <Logo />
       <NavLinks />
       <CTA />
@@ -9,13 +11,40 @@ export default function Header() {
 }
 
 function Logo() {
-  return <div>Logo</div>;
+  return (
+    <div className="flex items-center gap-2">
+      <div className="size-8 rounded-lg bg-linear-45 from-[#EC4899] to-[#7C3AED] text-white font-bold flex items-center justify-center">
+        DS
+      </div>
+      <span className="font-bold">
+        Dev <span className="text-[#DB2777]">Stack</span>
+      </span>
+    </div>
+  );
 }
 
 function NavLinks() {
-  return <div>NavLinks</div>;
+  return (
+    <nav>
+      <ul className="flex gap-4 text-sm">
+        {NAV_LINKS.header.map((link, index) => (
+          <li key={link}>
+            <a
+              href="javascript:void(0)"
+              className={`${index == 0 ? "text-[#DB2777] font-semibold" : "text-[#475569] fotn-medium"}`}
+            >
+              {link}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 function CTA() {
-  return <div>CTA</div>;
+  return <div>
+    <button className="px-4 py-2 rounded-full text-sm">Sign In</button>
+    <button className="bg-[#D91B7E] px-4 py-2 text-white rounded-full text-sm">Sign Up</button>
+  </div>;
 }
